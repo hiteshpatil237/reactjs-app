f25f2344b15f1f57b515f1ae9d44aab7c037c1c3
hiteshpatil237 <hiteshpatil237@gmail.com>
2024-07-11 22:24:25 -0400

    Added css for the UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import DetailsCardComponent from "./components/DetailsCardComponent";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import './App.css'
 
 function App() {
   const [formData, setFormData] = useState({ name: "", email: "" });
